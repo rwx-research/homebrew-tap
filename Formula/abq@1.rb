776e8803ff5809e8b1ba44d5bc84f3b2dd790036
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2023-01-24 08:29:22 -0600

    Bump tap to 1.0.6

--- Formula/abq@1.rb
+++ Formula/abq@1.rb
@@ -1,16 +1,16 @@
 class AbqAT1 < Formula
   desc "Always Be Queueing - Run your tests in parallel"
   homepage "https://abq.build/"
-  version "1.0.4"
+  version "1.0.6"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
-    sha256 "96d562af076490fde75996de49ecdf353827718cf95269751dfb5225da3e7b38"
+    sha256 "1623684aca943b9fe5974d0f0938504ff085fc739729391d48b542848c3e6e65"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
-    sha256 "ea2ba717db5c94799bbf6432e20d6b156011a91ca3eb5ce041281d35b6cdfe58"
+    sha256 "f75e1001304eb1584e7be26328885d6797f20b0fe2f9121da3fa694af194df4e"
   end
 
   def install
