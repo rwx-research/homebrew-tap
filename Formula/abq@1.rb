458a1f56817e9fe6899e135390014172aa3587a3
Michael Glass <me@mike.is>
2023-02-10 16:20:33 +0100

    bump to 1.1.0

--- Formula/abq@1.rb
+++ Formula/abq@1.rb
@@ -1,16 +1,16 @@
 class AbqAT1 < Formula
   desc "Always Be Queueing - Run your tests in parallel"
   homepage "https://abq.build/"
-  version "1.0.9"
+  version "1.1.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
-    sha256 "db2ec1cb61b50aac5682cd2fa08ea43f5b346ddee80c12122974cd2c684689af"
+    sha256 "d64ae67e312bed35c5c6f2d83d8ebc11b8a167f147ecc61ba439c2e0911ff6d6"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
-    sha256 "d6b7d75a17d5e6f9c9cc5450695b63872cdb4d28c2035ca71b6dc94631c31661"
+    sha256 "5dc7cde11d9e071207e63d4aa4a845ad3120c59367f7314a72bb2630dae5ce42"
   end
 
   def install
