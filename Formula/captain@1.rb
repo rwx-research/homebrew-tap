7f43793d91642511624edcad9f9526ec3d467abd
Kyle Thompson <me@kkt.dev>
2023-04-07 11:32:41 -0400

    Release v1.10.0

--- Formula/captain@1.rb
+++ Formula/captain@1.rb
@@ -1,16 +1,16 @@
 class CaptainAT1 < Formula
   desc "Captain - CI and Test Analytics"
   homepage "https://captain.build"
-  version "1.9.4"
+  version "1.10.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://releases.captain.build/v#{version}/darwin/x86_64/captain", user_agent: :fake
-    sha256 "37d2c4540c3c68625ed726f615af2f94c4ac41da1f109a922eb84a0876426d4d"
+    sha256 "7b863dac3b1dd2618b00b2ae1842f9ff7f4ab353540ac49af7b499e2597b7b86"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://releases.captain.build/v#{version}/darwin/aarch64/captain", user_agent: :fake
-    sha256 "e558b03c9fea63b8fa74001f523e3ba3c47ca6c181f628455a319ecbd694da1d"
+    sha256 "afc375ae50ae00505aaa28e0ac8ab954f14ca2410113e551ebd821e843d42c31"
   end
 
   def install
