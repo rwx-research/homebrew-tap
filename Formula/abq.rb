abeb43d8c4a089facc62dd6becb0f01b88f4686d
Dan Manges <dan@rwx.com>
2023-05-08 20:43:29 -0400

    Update ABQ to 1.4.0

--- Formula/abq.rb
+++ Formula/abq.rb
@@ -1,16 +1,16 @@
 class Abq < Formula
   desc "Always Be Queueing - Run your tests in parallel"
   homepage "https://abq.build"
-  version "1.3.0"
+  version "1.4.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
-    sha256 "9a644fcf64887c6979b7b7288cab8fd6bbba1dc9b21f8eafca4bdb75373babd2"
+    sha256 "eecc41dcb1b4f1215275b475fb5463ab3d20055281f38a9e951b2409fd917a01"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
-    sha256 "b9057a8385a48a233b941b44b36d077b9d821fe15b2700acb4a39c8aa7d1ec76"
+    sha256 "83a4b6027705aa38a82c00a8c1f33a86dd8c32a9e08b5a20ad7bc0a4513c2523"
   end
 
   def install
