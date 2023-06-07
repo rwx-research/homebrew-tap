3e86fc807d39ae6fe5ba4666c33795693bb05e4c
Michael Glass <me@mike.is>
2023-06-07 14:40:47 +0200

    upgrade abq to 1.5.0 (#33)

--- Formula/abq@1.rb
+++ Formula/abq@1.rb
@@ -1,16 +1,16 @@
 class AbqAT1 < Formula
   desc "Always Be Queueing - Run your tests in parallel"
   homepage "https://abq.build"
-  version "1.4.0"
+  version "1.5.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
-    sha256 "81f57ad5fdcfd6f8819d6b192ec254809049b33ac9516f03002553876d2fb850"
+    sha256 "b16eb4e6550ca77d6156adcefd1b72af1abf29b9759e51d6799e82a0225efae4"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
-    sha256 "550a721866c98bc3d3bec201bb874ee075adbd8e8664d667e19020d37d99976c"
+    sha256 "7ef1dac1d4e92c6cb8bc746b38c673cc19a739bc3b5a96addd85ec198c89a3e4"
   end
 
   def install
