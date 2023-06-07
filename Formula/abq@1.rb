206a8f800c1cd0b7012c6b380340b9a4096d28c5
Michael Glass <me@mike.is>
2023-06-07 14:52:49 +0200

    fix shas that got accidentally changed (#34)

--- Formula/abq@1.rb
+++ Formula/abq@1.rb
@@ -5,12 +5,12 @@ class AbqAT1 < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
-    sha256 "b16eb4e6550ca77d6156adcefd1b72af1abf29b9759e51d6799e82a0225efae4"
+    sha256 "32000f5989c2d7a6a3092628709d246ac76cdaf7756cc10347aeb7102d440016"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://abq.build/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
-    sha256 "7ef1dac1d4e92c6cb8bc746b38c673cc19a739bc3b5a96addd85ec198c89a3e4"
+    sha256 "fef0c14e2117ae9f78531877c6eb3a077094cbdf2fd00488de8279cb3722c279"
   end
 
   def install
