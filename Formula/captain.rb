dc11d49aa3cadac2d63b90ed64a7924ee9c08f71
Michael Glass <me@mike.is>
2023-07-11 14:51:16 +0200

    update captain to v1.11.2 (#36)

--- Formula/captain.rb
+++ Formula/captain.rb
@@ -1,16 +1,16 @@
 class Captain < Formula
   desc "Captain - CI and Test Analytics"
   homepage "https://captain.build"
-  version "1.11.1"
+  version "1.11.2"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://releases.captain.build/v#{version}/darwin/x86_64/captain", user_agent: :fake
-    sha256 "9c76b0ac3203d393b1f5e533cf7c0e85c2ebd59f3eee91cf043ca53ccf4d6ba5"
+    sha256 "dc8e6b4d44896ea8b8b3d164445ef7c13e24b76428de6b01195aa2cac217850b"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://releases.captain.build/v#{version}/darwin/aarch64/captain", user_agent: :fake
-    sha256 "d32ebc3d2801320d4e850f64e1c61dc1800043b17b57551caae3d6c8b115dd43"
+    sha256 "2eeb108b344a785ff19c395668ca2a0af0c3ec2b82dacaeb2740483d17d09350"
   end
 
   def install
