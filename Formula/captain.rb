d06b035d3a1f43b9950f41448e345aca43a7a180
Ayaz <20735482+ayazhafiz@users.noreply.github.com>
2023-10-19 09:55:15 -0400

    Bump captain to 1.11.4 (#39)

--- Formula/captain.rb
+++ Formula/captain.rb
@@ -1,16 +1,16 @@
 class Captain < Formula
   desc "Captain - CI and Test Analytics"
   homepage "https://captain.build"
-  version "1.11.2"
+  version "1.11.4"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://releases.captain.build/v#{version}/darwin/x86_64/captain", user_agent: :fake
-    sha256 "dc8e6b4d44896ea8b8b3d164445ef7c13e24b76428de6b01195aa2cac217850b"
+    sha256 "f364dea262c1e420af094493dde2bd698031fe7508ab632cf2bcaab288420329"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://releases.captain.build/v#{version}/darwin/aarch64/captain", user_agent: :fake
-    sha256 "2eeb108b344a785ff19c395668ca2a0af0c3ec2b82dacaeb2740483d17d09350"
+    sha256 "54d60c09f30fdb6296759d7bca9c01936ab939e175a97ae67fad54703de688b8"
   end
 
   def install
