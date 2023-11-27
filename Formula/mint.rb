9797598d610d8203d766d7e0d6301bed71dc1264
Tommy Graves <tommy@rwx.com>
2023-11-27 16:03:53 -0500

    Fixes the update-mint-formulate script

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,21 +1,21 @@
 class Mint < Formula
-  desc 'Mint - A language-agnostic build tool designed to accelerate engineering feedback loops and lower CI costs'
-  homepage 'https://www.rwx.com/mint'
-  version '0.0.2'
+  desc "Mint - A language-agnostic build tool designed to accelerate engineering feedback loops and lower CI costs"
+  homepage "https://www.rwx.com/mint"
+  version "0.0.2"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-x86_64", user_agent: :fake
-    sha256 'ff9297ee2273d26032fe1ed472362c01db3d760481f54c4c360dda58cdd3de1b'
+    sha256 "ff9297ee2273d26032fe1ed472362c01db3d760481f54c4c360dda58cdd3de1b"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-aarch64", user_agent: :fake
-    sha256 'a81114b15f96e5d0d7c881bfe91e807295d0ad16b1d7de042cc5cd479772ee54'
+    sha256 "a81114b15f96e5d0d7c881bfe91e807295d0ad16b1d7de042cc5cd479772ee54"
   end
 
   def install
-    filename = Hardware::CPU.intel? ? 'mint-darwin-x86_64' : 'mint-darwin-aarch64'
-    bin.install filename => 'mint'
+    filename = Hardware::CPU.intel? ? "mint-darwin-x86_64" : "mint-darwin-aarch64"
+    bin.install filename => "mint"
   end
 
   test do
