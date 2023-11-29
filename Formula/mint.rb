169a0f947425e9e557e33cd397b2740c1074a6e2
Tommy Graves <tommy@rwx.com>
2023-11-29 12:13:39 -0500

    Update Mint to v0.0.4

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,16 +1,16 @@
 class Mint < Formula
   desc "Mint - A language-agnostic build tool designed to accelerate engineering feedback loops and lower CI costs"
   homepage "https://www.rwx.com/mint"
-  version "0.0.3"
+  version "0.0.4"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-x86_64", user_agent: :fake
-    sha256 "736aae8d0999078cc17e3bd1f3217ea80ff3a898575abb8be8bde4a9243780a5"
+    sha256 "17bf37a5e0573430d6fdf8f78aa6752ff1e456cd38b51ab6669991fb3c6689a5"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-aarch64", user_agent: :fake
-    sha256 "5a774ab21fa6a73a2783b9206f9b4d23844564920d4c811f71d2b52055df41cd"
+    sha256 "2362d267d1c68422adb9f67268c327b431da98cd3838cb97fcd5aa57ee956165"
   end
 
   def install
