77dc9e4dc737681a3a7cfe4e766cc97e0a30d615
Tony Schneider <tony@rwx.com>
2024-01-05 14:34:10 -0500

    Update abq api to use cloud host

--- Formula/abq.rb
+++ Formula/abq.rb
@@ -4,12 +4,12 @@ class Abq < Formula
   version "1.6.3"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://abq.build/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
+    url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
     sha256 "e1618b74a15340ba0503ed83b592ef6da8e2b6d39231baabb1507ad95370578c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://abq.build/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
+    url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
     sha256 "9bf2086f087c93e94461c12711f52bb0eb316df693367d91169aac4b5a707802"
   end
 
