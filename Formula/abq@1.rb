1c62446871b8e50645025506a9d838785d82f897
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2024-01-16 07:37:41 -0800

    Bump to ABQ 1.7.3

--- Formula/abq@1.rb
+++ Formula/abq@1.rb
@@ -1,16 +1,16 @@
 class AbqAT1 < Formula
   desc "ABQ is the most efficient parallel test runner due to its queue-based distribution"
   homepage "https://www.rwx.com/abq"
-  version "1.7.1"
+  version "1.7.3"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
-    sha256 "5386947f49db857252a1a1dc6cfd0dd12a1a2826f3d79dfb68121c5af202f786"
+    sha256 "544d303b33f53ffcf0552dd71f6c01d5a681dbb2bcd62ddccf490f7391bc4c26"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
-    sha256 "5163cefa68b462186bd001707bb84f79b87461502dfa4b67b2dad4f018cab983"
+    sha256 "bd777ff0f6d4356a7618eb0f01283da54da24a62d6c466d0c4935d3334a016f4"
   end
 
   def install
