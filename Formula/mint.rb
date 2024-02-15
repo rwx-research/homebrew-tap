530b9650d98a0121c2f89001707fd4c65e8283d7
Tommy Graves <tommy@rwx.com>
2024-02-15 09:31:39 -0500

    Update Mint to 0.1.1 (#66)

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,16 +1,16 @@
 class Mint < Formula
   desc "Mint is the CI platform with the best developer experience, powering the fastest builds"
   homepage "https://www.rwx.com/mint"
-  version "0.1.0"
+  version "0.1.1"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-x86_64", user_agent: :fake
-    sha256 "43b370bffa524a33246e7512f2be03f680d4ebd428e09bca87256eb5b88c3ed1"
+    sha256 "8c13b18ebad7b2afa7490ea2aabb3c2a21a5090891d56f01543a9fd905f9ddae"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-aarch64", user_agent: :fake
-    sha256 "b7c7d33a6c990b216e89f939252f7a0a42dc9307df4f32238ca125f3b6975359"
+    sha256 "3f3f6617ac327ceeef41cb0c3a1e65e7880c7210d9bcd00b2fc2c4948f4de6ab"
   end
 
   def install
