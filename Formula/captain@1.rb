ebe75402e2a9f3f2d24a24a13d5ab4ce658dac77
Dan Manges <dan@rwx.com>
2024-02-26 22:37:56 -0500

    Add captain for linux

--- Formula/captain@1.rb
+++ Formula/captain@1.rb
@@ -3,14 +3,19 @@ class CaptainAT1 < Formula
   homepage "https://www.rwx.com/captain"
   version "1.11.7"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.captain.build/v#{version}/darwin/x86_64/captain", user_agent: :fake
-    sha256 "7f9be5df82790bf265628a92936cf0d851045b6a4e8c22e5af31ad5930a068c1"
-  end
-
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.captain.build/v#{version}/darwin/aarch64/captain", user_agent: :fake
-    sha256 "d7e2255f01e7fe7711daeeb13a99b68599aef7e9cd40903d97aefdfd70fbcf48"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://releases.captain.build/v#{version}/darwin/x86_64/captain", user_agent: :fake
+      sha256 "7f9be5df82790bf265628a92936cf0d851045b6a4e8c22e5af31ad5930a068c1"
+    elsif Hardware::CPU.arm?
+      url "https://releases.captain.build/v#{version}/darwin/aarch64/captain", user_agent: :fake
+      sha256 "d7e2255f01e7fe7711daeeb13a99b68599aef7e9cd40903d97aefdfd70fbcf48"
+    end
+  else
+    if Hardware::CPU.intel?
+      url "https://releases.captain.build/v#{version}/linux/x86_64/captain", user_agent: :fake
+      sha256 "666c7f6639559a3070273041b693eabfb54168a2a5fcc83c173648d2f6811e10"
+    end
   end
 
   def install
