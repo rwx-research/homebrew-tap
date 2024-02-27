89fb48a22a9b3666401b8295b640f7d54cb81b68
Dan Manges <dan@rwx.com>
2024-02-26 22:40:59 -0500

    Add mint for linux

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -3,18 +3,27 @@ class Mint < Formula
   homepage "https://www.rwx.com/mint"
   version "0.1.1"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-x86_64", user_agent: :fake
-    sha256 "8c13b18ebad7b2afa7490ea2aabb3c2a21a5090891d56f01543a9fd905f9ddae"
-  end
-
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-aarch64", user_agent: :fake
-    sha256 "3f3f6617ac327ceeef41cb0c3a1e65e7880c7210d9bcd00b2fc2c4948f4de6ab"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-x86_64", user_agent: :fake
+      sha256 "8c13b18ebad7b2afa7490ea2aabb3c2a21a5090891d56f01543a9fd905f9ddae"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-aarch64", user_agent: :fake
+      sha256 "3f3f6617ac327ceeef41cb0c3a1e65e7880c7210d9bcd00b2fc2c4948f4de6ab"
+    end
+  else
+    if Hardware::CPU.intel?
+      url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-linux-x86_64", user_agent: :fake
+      sha256 "88f46ba7b54d914b63968be346fc0f1002429749683c651f9bc256775aac40df"
+    end
   end
 
   def install
-    filename = Hardware::CPU.intel? ? "mint-darwin-x86_64" : "mint-darwin-aarch64"
+    filename = case
+                 when OS.mac? && Hardware::CPU.intel? then "mint-darwin-x86_64"
+                 when OS.mac? && Hardware::CPU.arm? then "mint-darwin-aarch64"
+                 when OS.linux? && Hardware::CPU.intel? then "mint-linux-x86_64"
+               end
     bin.install filename => "mint"
   end
 
