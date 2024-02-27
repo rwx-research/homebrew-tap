5e6a16d3c26eebb393dd75f3f8b1d4fe8717a1e2
Dan Manges <dan@rwx.com>
2024-02-26 21:50:25 -0500

    Add support for linux

--- Formula/abq@1.rb
+++ Formula/abq@1.rb
@@ -3,14 +3,18 @@ class AbqAT1 < Formula
   homepage "https://www.rwx.com/abq"
   version "1.7.4"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
-    sha256 "d76b514dc34ea7322f463b7e470e3cea0305d72a11ff81cc591aea1ddbb20e70"
-  end
-
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
-    sha256 "818d48e9cdae3e88718affbafaa34cd0a474beea097c7ce0e5fdba5e3fa2a1ed"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
+      sha256 "d76b514dc34ea7322f463b7e470e3cea0305d72a11ff81cc591aea1ddbb20e70"
+    elsif Hardware::CPU.arm?
+      url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
+      sha256 "818d48e9cdae3e88718affbafaa34cd0a474beea097c7ce0e5fdba5e3fa2a1ed"
+    else
+      depends_on arch: [:intel, :arm64]
+    end
+  else
+    depends_on :macos
   end
 
   def install
