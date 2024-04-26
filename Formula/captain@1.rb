d3060f83adce4119dc3b32a3c5eaa53d43ed8d7a
Kyle Thompson <me@kkt.dev>
2024-04-26 15:19:02 -0400

    Update captain to 1.12.0

--- Formula/captain@1.rb
+++ Formula/captain@1.rb
@@ -1,20 +1,20 @@
 class CaptainAT1 < Formula
   desc "Captain can detect and quarantine flaky tests, automatically retry failed tests, partition files for parallel execution, and more"
   homepage "https://www.rwx.com/captain"
-  version "1.11.8"
+  version "1.12.0"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://releases.captain.build/v#{version}/darwin/x86_64/captain", user_agent: :fake
-      sha256 "bf1de5d9ea02514f38bc7fd558ede07f9f6207649aa7b3588fef833859a6719b"
+      sha256 "17c6e434c879bbb6fa370cdaab6614d5f2c3a53b116c94e28e6d050e7d83f4fa"
     elsif Hardware::CPU.arm?
       url "https://releases.captain.build/v#{version}/darwin/aarch64/captain", user_agent: :fake
-      sha256 "5e36b34b6554d34b0d5aef6a8cd5a9d075a0579c86916f124c4f0d09b039fac1"
+      sha256 "3c3b1c03c33166e05d8226d959223588626217f9314a028a37a539c6b412ab31"
     end
   else
     if Hardware::CPU.intel?
       url "https://releases.captain.build/v#{version}/linux/x86_64/captain", user_agent: :fake
-      sha256 "a8af58ccc380d12b076f24ee8533ff4d92f1f39088925ea375750e68b8a3575d"
+      sha256 "b98d6123c27fe949dea8c910a7bf106aaff445d7bf57bdd363a23fa4b8e8ba1a"
     end
   end
 
