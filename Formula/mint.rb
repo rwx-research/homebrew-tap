6b8a8f5562a3b811df8609766223f7139df1d08f
Dan Manges <dan@rwx.com>
2024-05-22 10:03:15 -0400

    Update Mint to v0.2.2 (#75)

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,20 +1,20 @@
 class Mint < Formula
   desc "Mint is the CI platform with the best developer experience, powering the fastest builds"
   homepage "https://www.rwx.com/mint"
-  version "0.2.1"
+  version "0.2.2"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-x86_64", user_agent: :fake
-      sha256 "9bc8885d9c6e55e9136a65ec741118f7b154849d2bc3c84f801b672007527fa7"
+      sha256 "46e29935f3e638d82e684813e80d6d058ed81ed3fde70d079a3318747fb947dd"
     elsif Hardware::CPU.arm?
       url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-aarch64", user_agent: :fake
-      sha256 "87b841c56d7e01ae12531fd01ddc16cbd8b2281ba96e6ece493105e789148847"
+      sha256 "b09d5205d48c91a411612101fc1b94682b1853612eecf0376da3e5c0d4cdfead"
     end
   else
     if Hardware::CPU.intel?
       url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-linux-x86_64", user_agent: :fake
-      sha256 "6b0c3c13ccbb2212d5c86afa7f81b1e73b83fc113b6b2ad67722fb8251fa3a32"
+      sha256 "b791e9fabd6ad5b31a30392b4a935de5bb0ff20127311cc058b560a91f289ba9"
     end
   end
 
