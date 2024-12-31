6c570ceace2a7f98265ce7037d0986b85e1aa25b
Tommy Graves <tommy@rwx.com>
2024-12-31 09:22:21 -0500

    Release ABQ 1.9.2

--- Formula/abq@1.rb
+++ Formula/abq@1.rb
@@ -1,20 +1,20 @@
 class AbqAT1 < Formula
   desc "ABQ is the most efficient parallel test runner due to its queue-based distribution"
   homepage "https://www.rwx.com/abq"
-  version "1.9.1"
+  version "1.9.2"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=x86-64", user_agent: :fake
-      sha256 "c83e9cced9e0ff55ee53761b8bdfb11f91e1e5863ca282b616efdcb6f0e58c58"
+      sha256 "e719870a610733251bf68573baae2e68cab5e93d51dd9e0824494e88466d00cb"
     elsif Hardware::CPU.arm?
       url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=darwin&arch=aarch64", user_agent: :fake
-      sha256 "d3a05f6025f7d245c4484c7a4bed8b45018a570db4f8630ae96d714a2bf63702"
+      sha256 "aef5e6fb3d38566cc95d45925a3220e07fc6d8ea7413b055af70a5c33b30f91a"
     end
   else
     if Hardware::CPU.intel?
       url "https://cloud.rwx.com/abq/api/downloads/#{version}?os=linux&arch=x86-64", user_agent: :fake
-      sha256 "08a7d56b9856264771bb4fa4841b28ec185a29c828d72478452b4ac5e56249e9"
+      sha256 "6927119baf463120c497bb41006a9d3dd4416231d7b35483c73a0e46ace8cfe5"
     end
   end
 
