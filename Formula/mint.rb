e081c395a18385dbf59b850e648e5ff3a32b1def
Doug Mayer <doug@rwx.com>
2025-05-05 08:48:41 -0500

    Release Mint 1.5.0 (#109)

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,20 +1,20 @@
 class Mint < Formula
   desc "Mint is the CI platform with the best developer experience, powering the fastest builds"
   homepage "https://www.rwx.com/mint"
-  version "1.4.1"
+  version "1.5.0"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-x86_64", user_agent: :fake
-      sha256 "197c17341dcce43e65ffbda599f564132c27b53b2a402ccc7192812c918c822e"
+      sha256 "a646cda014a7b7686aaafff50e7cc15bc35f12f0ee4b2dbee8b971645bcb0b07"
     elsif Hardware::CPU.arm?
       url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-aarch64", user_agent: :fake
-      sha256 "28be21968f4bd1a39f6f879552b9b0f97bf128bc663ac5b14215b5bbd3a89e95"
+      sha256 "8872b6e0124335c47362deb503210cde4893429e09d73680da60bf778b5eba47"
     end
   else
     if Hardware::CPU.intel?
       url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-linux-x86_64", user_agent: :fake
-      sha256 "f85b7c1c6483e987d02669f1f1241f1b2a651751ef80c45b2f6d127ae1f07044"
+      sha256 "99a5fb0a52c5cb7443add84df0de2953a54371cc13e9196e950328779319e4ab"
     end
   end
 
