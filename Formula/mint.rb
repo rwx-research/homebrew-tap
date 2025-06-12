c8da67e916986121e693edef9aa49ace6f0abdbf
Kyle Thompson <me@kkt.dev>
2025-06-12 13:05:06 -0400

    Release Mint 1.6.0 (#115)

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,20 +1,20 @@
 class Mint < Formula
   desc "Mint is the CI platform with the best developer experience, powering the fastest builds"
   homepage "https://www.rwx.com/mint"
-  version "1.5.1"
+  version "1.6.0"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-x86_64", user_agent: :fake
-      sha256 "a578ded2640e00410e98dc9e4d8746adc8740e8c68fb8719280d885f8999eefd"
+      url "https://github.com/rwx-cloud/cli/releases/download/v#{version}/mint-darwin-x86_64", user_agent: :fake
+      sha256 "e0918897a95a6fa09151d99df0db45ead5a06ec5081e42f88f0867f7e13acb51"
     elsif Hardware::CPU.arm?
-      url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-darwin-aarch64", user_agent: :fake
-      sha256 "5d539b9f9e1454c51c58240ab10b0650ca97501ab469065a65c9a8ce58365404"
+      url "https://github.com/rwx-cloud/cli/releases/download/v#{version}/mint-darwin-aarch64", user_agent: :fake
+      sha256 "97cbd53dce4d63ff06331a390844b346b6470404d4e5aa09f8c3c01f9725b21b"
     end
   else
     if Hardware::CPU.intel?
-      url "https://github.com/rwx-research/mint-cli/releases/download/v#{version}/mint-linux-x86_64", user_agent: :fake
-      sha256 "53759725887e6feb1f5451d995e673e83b429123b113b815501abe5461293a11"
+      url "https://github.com/rwx-cloud/cli/releases/download/v#{version}/mint-linux-x86_64", user_agent: :fake
+      sha256 "958ca3e6b4bccb050574312d821c3e041540024ca98e6aa6397caaecd2d1bfb1"
     end
   end
 
